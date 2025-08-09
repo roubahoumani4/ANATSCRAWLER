907c554c9de3458673162b97f5084217452a43ac
Krikorios <99836218+Krikorios@users.noreply.github.com>
2025-08-09 04:50:37 +0300

    changes2 by georges

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -10,7 +10,11 @@ module.exports = {
         MONGODB_URL: 'mongodb://192.168.1.110:27017/anat_security',
         REDIS_URL: 'redis://192.168.1.110:6379',
         JWT_SECRET: process.env.JWT_SECRET,
-        COOKIE_SECRET: process.env.COOKIE_SECRET
+        COOKIE_SECRET: process.env.COOKIE_SECRET,
+        MISP_URL: 'https://hera.anatsecurity.fr',
+        MISP_API_KEY: 'A9ODR6gV6aYz55sPpwK8vzHbdq2bgGHzzsW4kO5D',
+        MISP_VERIFY_TLS: process.env.MISP_VERIFY_TLS,
+        MISP_TIMEOUT_MS: process.env.MISP_TIMEOUT_MS
       }
     }
   ]
